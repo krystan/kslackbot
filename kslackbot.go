5b9650599a25552f0fc6f4f8d96982c4a41537e9
Krystan Honour <krystan@forward-thinking-it.com>
2017-09-19 20:47:10 +0100

    added functionality to check git hub, and reindented files

--- kslackbot.go
+++ kslackbot.go
@@ -1,12 +1,70 @@
-package main
+  package main
 
-import (
-	"fmt"
-	"os"
-)
+  import (
+    "os"
+    "fmt"
+    "log"
+    "strings"
+    "github.com/google/go-github/github"
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Fprintf(os.Stderr, "usage: kslackbot <args>\n")
-	}
-}
+    "context"
+  )
+
+  func main() {
+    if len(os.Args) != 2 {
+      fmt.Fprintf(os.Stderr, "usage: kslackbot <args>\n")
+      return
+    }
+
+    // git client
+    gitClient := github.NewClient(nil)
+
+    // open connection
+    ws, id := connectToSlack(os.Args[1])
+    fmt.Print("bot online, hit ^C to exit")
+
+    gitClient = github.NewClient(nil)
+
+    for {
+      m, err := getMessage(ws)
+
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      // check for message id
+      if m.Type == "message" && strings.HasPrefix(m.Text, "<@"+id+">") {
+        parts := strings.Fields(m.Text)
+
+        if len(parts) == 3 && parts[1] == "commit" {
+          go func(m SlackMessage) {
+            commit,err := getLastCommit("krystan", parts[2],  gitClient)
+
+            if err != nil {
+              log.Fatal("fatal error getting commit:%v", err)
+            }
+
+            m.Text = commit
+
+            sendMessage(ws, m)
+          }(m)
+        } else {
+          m.Text = fmt.Sprintf("unexpected response\n")
+          sendMessage(ws, m)
+        }
+      }
+    }
+  }
+
+
+  func getLastCommit(owner, repo string, sgc *github.Client) (string, error) {
+    ctx := context.Background()
+    commits, res, err := sgc.Repositories.ListCommits(ctx, owner, repo, &github.CommitsListOptions{})
+
+    if err != nil {
+      log.Printf("err: %s res: %s", err, res)
+      return "", err
+    }
+
+    return *commits[0].SHA, nil
+  }
---
+++ slack.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "encoding/json"
+    "sync/atomic"
+    "log"
+    "net/http"
+
+    "golang.org/x/net/websocket"
+)
+
+type rtmStartResponse struct {
+  Ok    bool         `json:"ok"`
+  Error string       `json:"error"`
+  Url   string       `json:"url"`
+  Self  responseSelf `json:"self"`
+}
+
+type responseSelf struct {
+  Id string `json:"id"`
+}
+
+func slackStart(token string) (wsurl, id string, err error) {
+  url := fmt.Sprintf("https://slack.com/api/rtm.start?token=%s", token)
+  resp, err := http.Get(url)
+
+  if err != nil {
+    return
+  }
+
+  if resp.StatusCode != 200 {
+    err = fmt.Errorf("Request failed with code %d", resp.StatusCode)
+    return
+  }
+
+  body, err := ioutil.ReadAll(resp.Body)
+  resp.Body.Close()
+
+  if err != nil {
+    return
+  }
+
+  var responseBody rtmStartResponse
+  err = json.Unmarshal(body, &responseBody)
+
+  if err != nil {
+    return
+  }
+
+  if !responseBody.Ok {
+    err = fmt.Errorf("Slack error: %s", responseBody.Error)
+    return
+  }
+
+  wsurl = responseBody.Url
+  id = responseBody.Self.Id
+
+  return
+}
+
+
+type SlackMessage struct {
+  Id      uint64 `json:"id"`
+  Type    string `json:"type"`
+  Channel string `json:"channel"`
+  Text    string `json:"text"`
+}
+
+func getMessage(ws *websocket.Conn) (m SlackMessage, err error) {
+  err = websocket.JSON.Receive(ws, &m)
+  return
+}
+
+var messageCount uint64
+func sendMessage(ws *websocket.Conn, m SlackMessage) error {
+  m.Id = atomic.AddUint64(&messageCount, 1)
+  return websocket.JSON.Send(ws, m)
+}
+
+func connectToSlack(token string) (*websocket.Conn, string) {
+  wsurl, id, err := slackStart(token)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  ws, err := websocket.Dial(wsurl, "", "https://api.slack.com/")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return ws, id
+}
